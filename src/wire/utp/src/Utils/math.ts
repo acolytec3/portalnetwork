2d7e9a9e71ae372eb3f377590f16a9795fa50c88
ScottyPoi <kyzambo@gmail.com>
2021-11-18 20:13:01 -0700

    maybe fix for packetToBuffer

--- src/wire/utp/src/Utils/math.ts
+++ src/wire/utp/src/Utils/math.ts
@@ -69,8 +69,8 @@ export function getMonoTimeStamp(): Uint32 {
 
   export function packetToBuffer(packet: Packet): Buffer {
     let buffer = Buffer.alloc(20 + (packet.payload ? packet.payload.length : 0))
-    let p = packet.header.pType.toString();
-    let v = packet.header.version.toString();
+    let p = packet.header.pType.toString(16);
+    let v = packet.header.version.toString(16);
     let pv = p + v;
     let typeAndVer = parseInt(pv,16)
 
